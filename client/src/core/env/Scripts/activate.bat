0dd51d0698b971cfe12be5cdcbfa8bf1d64d1a73
Edward <echang49@uwo.ca>
2021-01-01 18:06:36 -0500

    Remade virtualenv

--- client/src/core/env/Scripts/activate.bat
+++ client/src/core/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Edward\Documents\GitHub\CV-Creator\bin\core\env
+set VIRTUAL_ENV=C:\Users\Edward\Documents\GitHub\CV-Creator\client\src\core\env
 
 if not defined PROMPT set PROMPT=$P$G
 
