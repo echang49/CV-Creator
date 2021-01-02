d2e988a7c214180fdbb61e04b70d1d96b5b09c05
Edward <echang49@uwo.ca>
2021-01-01 21:37:30 -0500

    electron fullscreen
    maximizes electron on launch and sets minimum supported resolution to be 1080p.

--- client/src/electron-starter.js
+++ client/src/electron-starter.js
@@ -16,14 +16,15 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: { nodeIntegration: true, enableRemoteModule: true }});
+    mainWindow = new BrowserWindow({minWidth: 1920, minHeight: 1080, width:1920, height:1080, webPreferences: { nodeIntegration: true, enableRemoteModule: true }});
+    mainWindow.maximize();
     mainWindow.loadURL('http://localhost:3000');
     // mainWindow.loadURL(url.format({
     //     pathname: path.join(__dirname, '/../build/index.html'),
     //     protocol: 'file:',
     //     slashes: true
     // }));
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
     mainWindow.on('closed', function () {
         mainWindow = null
     })
